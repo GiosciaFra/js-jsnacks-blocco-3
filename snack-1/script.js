19f7add6c0b6030c12226a66895fe83e1fd5bac6
giosciafra <francescogioscia8@gmail.com>
2024-02-12 12:44:56 +0100

    snack 1 terminato in console

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,2 +1,19 @@
 // Snack 1:
 // Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
+
+
+const string = "Ciao";
+
+const turnedString = reverseString(string);
+
+function reverseString(string) {
+    let resultString = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        resultString += string[i];
+    }
+
+    return resultString;
+}
+
+console.log(turnedString);
