c5c622fe7cfa2e768892c013ef64a6f584552181
giosciafra <francescogioscia8@gmail.com>
2024-02-12 12:30:50 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,2 @@
+// Snack 1:
+// Scrivi una funzione che accetti una stringa come argomento e la restituisca girata (es. "Ciao" -> "oaiC")
---
+++ snack-4/script.js
@@ -0,0 +1,13 @@
+// Snack 4 (bonus):
+// Scrivi una funzione  che accetti una stringa come parametro e restituisca la stringa con i caratteri ordinati alfabeticamente
+// Es: riordina("webmaster") :freccia_destra: "abeemrstw"
+// Snack 5 (bonus):
+// Scrivi una funzione che accetti una stringa come parametro e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
+// Es: funzioneEsempio("nel mezzo del cammin di nostra vita")  :freccia_destra:  "Nel Mezzo Del Cammin Di Nostra Vita"
+
+
+
+
+
+
+
