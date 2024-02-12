e07ee3395b374dc3d76c6da60b60e071512e21b9
giosciafra <francescogioscia8@gmail.com>
2024-02-12 18:46:12 +0100

    snack 3 finito (stampa in console)

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,9 +1,39 @@
 // Snack 3:
 // Scrivi una funzione che accetti tre argomenti:
 // un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
-// Es:
-// abbiamo un array così: arrayNumeri = [0, 1, 2, 3, 4, 5, 6]
+// Es: abbiamo un array così: arrayNumeri = [0, 1, 2, 3, 4, 5, 6]
 // La nostra funzione viene dichiarata ad esempio così function tagliaArray(array, posizioneMin, posizioneMax)
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “posizioneMin” e “posizioneMax”
 // Nel nostro caso, se la chiamassimo così tagliaArray(arrayNumeri, 2, 4) ci restituirebbe un array così composto: [2, 3, 4]
  
+
+const numbersArray =['0', '1', '2', '3', '4', '5', '6'];
+
+const trimmedArray = cutArray (numbersArray, 2, 4);
+
+function cutArray (array, positionMin, positionMax){
+
+
+    // controllo la validità delle posizioni
+    if(positionMin < 0 || positionMin > array.length - 1){
+        alert ('Posizione minima non valida');
+    } if(positionMax < 0 || positionMax > array.length - 1 ){
+        alert ('Posizione massima non valida');
+    } if (positionMin > positionMax) {
+        alert ("la posizione minima dev'essere minore o uguale alla posizione massima");
+    }
+
+    //creao un nuovo array vuoto
+
+    const trimmedArray = [];
+
+
+    // con il for aggiungo all'array tagliato i valori tra positionMin e PositionMax
+    for (let i= positionMin; i <= positionMax; i++) {
+        trimmedArray.push(array[i]);
+    }
+
+    return trimmedArray;
+}
+
+console.log(trimmedArray);
