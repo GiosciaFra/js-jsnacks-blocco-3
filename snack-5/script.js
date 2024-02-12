bccc5f7b6ac5232e5fe75d71603accb34f471047
giosciafra <francescogioscia8@gmail.com>
2024-02-12 20:22:03 +0100

    snack 5 (secondo bonus) terminato e snack FINITI!

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,3 +1,24 @@
 // Snack 5 (bonus):
 // Scrivi una funzione che accetti una stringa come parametro e restituisca la stessa stringa ma con le iniziali delle parole tutte in maiuscolo
 // Es: funzioneEsempio("nel mezzo del cammin di nostra vita")  :freccia_destra:  "Nel Mezzo Del Cammin Di Nostra Vita"
+
+function capitalInitials(string){
+
+    // convertiamo la stringa in un array di parole
+    const words = string.split(" ");
+
+    //con charAt convertiamo la prima lettera (0) di ogni parola in maiuscolo
+    const capitalWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+
+    return capitalWords.join(" ");
+}
+
+document.addEventListener("click", function() {
+
+    
+    const string = document.getElementById("string").value;
+    const capitalString = capitalInitials(string);
+
+
+    document.getElementById("result").textContent = capitalString;
+})
